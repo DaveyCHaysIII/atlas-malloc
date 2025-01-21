eda61c8ee18194bdb88e0976d7d3595266383b86
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2025-01-20 21:50:21 -0600

    added repo and main files

---
+++ malloc.h
@@ -0,0 +1,52 @@
+#ifndef MALLOC_H
+#define MALLOC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define ALIGNMENT 8
+#define HEADER_SIZE sizeof(mheader_t)
+#define NHEADER_SIZE sizeof(nmheader_t)
+#define ALIGN_SIZE(size) (((size) + (ALIGNMENT - 1)) & ~(ALIGNMENT - 1))
+
+enum flags
+{
+	ALLOCATED,
+	FREE
+};
+
+/**
+ * mheader_t - header for _malloc
+ * @size: the size of the block
+ * @flag: 1 for free, 0 for allocated
+ * @prev: pointer to the previous block
+ * @next: pointer to the next block
+ */
+
+typedef struct mheader_s
+{
+	size_t size;
+	uint64_t flag;
+	void *prev;
+	void *next;
+} mheader_t;
+
+/**
+ * nmheader_t - header for the naive malloc
+ * @size: the size of the block
+ *
+ */
+
+typedef struct nmheader_s
+{
+	size_t size;
+} nmheader_t;
+
+void *naive_malloc(size_t size);
+void *find_next_chunk(void *ptr, size_t chunk);
+void *_malloc(size_t size);
+void _free(void *ptr);
+
+#endif
---
+++ naive_malloc.c
@@ -0,0 +1,60 @@
+#include "malloc.h"
+
+/**
+ * naive_malloc - the naive implementation of malloc
+ * @size: size of allocation
+ *
+ * Return: void * to allocated memory in heap
+ */
+
+void *naive_malloc(size_t size)
+{
+	static void *heap_start;
+	static void *heap_end;
+	static size_t chunks;
+	nmheader_t chunk_header, new_header;
+	void *chunk;
+
+	if (!heap_start)
+	{
+		heap_start = sbrk(0);
+		heap_end = sbrk(4096);
+		chunks = 0;
+	}
+
+	size = ALIGN_SIZE(size);
+	chunk = find_next_chunk(ptr, chunks);
+	chunks++;
+
+	/* write chunk header */
+	chunk_header = (nmheader_t *)chunk;
+	chunk_header->size = size;
+
+	/* write rest of heap header */
+	new_header = (nmheader_t *)((char *)chunk + NHEADER_SIZE + size);
+        new_header->size = (char *)heap_end - (char *)new_header - NHEADER_SIZE;
+
+	return (void *)((char *)chunk + NHEADER_SIZE);
+}
+
+/**
+ * find_next_chunk - traverse the heap to find next free chunk
+ * @ptr: starting point of heap
+ * @chunks: number of chunks in heap
+ *
+ * Return: void * to next free chunk
+ */
+
+void *find_next_chunk(void *heap_start, size_t chunks)
+{
+	void *nptr;
+
+	nptr = heap_start;
+	while (chunks > 0)
+	{
+		nmheader_t *header = (nmheader_t *)nptr;
+       		nptr = (void *)((char *)nptr + header->size + NHEADER_SIZE);
+		chunks--;
+	}
+	return nptr;
+}
